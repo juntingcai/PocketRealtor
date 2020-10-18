2c4ae6a2c9be0d4fdda7cb133ff1dc25ef29bf87
Jerry <mchen81@dons.usfca.edu>
2020-10-18 02:47:12 -0700

    fix bugs

--- server/routes/users.js
+++ server/routes/users.js
@@ -13,7 +13,7 @@ router.get('/user/role/:userId/', User.getUserRole)
 
 router.post('/user/register', User.register);
 router.post('/user/login', User.login)
-router.post('/user/verifyuser', User.verifyToken ,User.verifyToken)
+router.post('/user/verifyuser', User.verifyToken ,User.testUserToken)
 router.post('/user/updatePassword', User.verifyToken, User.updatePassword)
 router.put('/user/updateProfile', User.verifyToken, User.updateProfile)
 router.put('/user/updateRole', User.verifyToken, User.updateRole)
