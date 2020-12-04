1ec09d2e29d5c7a09e5863b871ffb32f0805117d
Jerry <54089282+mchen81@users.noreply.github.com>
2020-12-04 02:07:14 -0800

    update config

--- server/config.js
+++ server/config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  listenPort : '443',
-  resetTables : false,
+  http_port : "80",
+  https_listenPort : "443",
+  alterTables : false,
   privary_key_pem: "/etc/letsencrypt/live/pocket-realtor.chickenkiller.com/privkey.pem",
   cert_pem: "/etc/letsencrypt/live/pocket-realtor.chickenkiller.com/cert.pem",
   chain_pem: "/etc/letsencrypt/live/pocket-realtor.chickenkiller.com/chain.pem"
